08c06aeaf995bf433e49708a133ce973b24e5137
Veeser,Yannik <yannik.veeser@mediserv.de>
2021-01-20 10:25:31 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using DBToolBox;
+
+namespace DBHelper
+{
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Data Source=WMS262;Initial Catalog=TestDB;Integrated Security=true";
+
+            SqlConnection cnn = new SqlConnection(connectionString);
+
+        }
+    }
+}
---
+++ TestObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBHelper
+{
+    class TestObject
+    {
+        public string Id;
+        public string Text;
+        public string AndereText;
+    }
+}
